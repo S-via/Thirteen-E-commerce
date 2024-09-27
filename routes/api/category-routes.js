8f983ba3560cf0d6544560e768ce7be2199ad335
S-via <168160925+S-via@users.noreply.github.com>
2024-09-27 16:42:01 -0700

    added async and await function for Category models

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,25 +6,58 @@ const { Category, Product } = require('../../models');
 router.get('/', async (req, res) => {
   // find all categories
   try{
-    const locationData = await Category.findAll();
+    // be sure to include its associated Products
+    const locationData = await Category.findAll(
+      {include:[{
+        model: Product,
+        required: false 
+      }]}
+    );
     res.status(200).json(locationData);
   }
   catch (err) {
     res.status(500).json(err);
   }
-  // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    // find one category by its `id` value
+    // be sure to include its associated Products
+    const locationData = await Category.findByPk(req.params.id,
+      {include :[{
+        model: Product,
+      required: false 
+    }]});
+      if (!locationData){
+        res.status(404).json({messegae: 'no location with id' });
+        return;
+      }
+      res.status(200).json(locationData);
+  }catch (err){
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try{
+    const locationData = await Category.create(req.body)
+
+    if(!locationData){
+      res.status(404).json({message:'new category added'});
+      return;
+    }
+    res.status(200).json(locationData);}
+  catch (err){
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+  try{
+    const locationData = await Category.update(req.params.id)
+  }
   // update a category by its `id` value
 });
 
