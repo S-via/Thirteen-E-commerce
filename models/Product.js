b8afe8b8fcd795ec07aecca4218c0987244121f4
S-via <168160925+S-via@users.noreply.github.com>
2024-09-28 12:44:38 -0700

    added id into model and category_id

--- models/Product.js
+++ models/Product.js
@@ -10,23 +10,38 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id:{
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     product_name:{
       type: DataTypes.STRING,
       alowNull:false,
     },
   
     price:{
-      type: DataTypes.DECIMAL(10,2),
+      type: DataTypes.DECIMAL,
       allowNul : false,
+      validate:{
+        isDecimal:true,
+      },
     },
+
     stock:{
       type: DataTypes.INTEGER,
       allowNull: false,
+      defaultValue:10,
+      validate:{
+        isNumeric:true,
+      },
     },
-    tagIds:{
+    
+    category_id:{
       type: DataTypes.INTEGER,
       references:{
-        model:'ProductTag', //?? or tag
+        model:'category', //this refrences the category model
         key:'id'
       }
     }
