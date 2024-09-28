26c085d2d58ac6dafca348a1ec6bc18dd4ba30f3
S-via <168160925+S-via@users.noreply.github.com>
2024-09-28 12:29:34 -0700

    in server.js i imported sequelize from config/connection and for product.js  i defined colums

--- models/Product.js
+++ models/Product.js
@@ -9,11 +9,11 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
+    // define columns
     product_name:{
       type: DataTypes.STRING,
       alowNull:false,
     },
-    // define columns
   
     price:{
       type: DataTypes.DECIMAL(10,2),
