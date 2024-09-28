c759ada89c17c0e5d1a62b52b5c89e84721b4272
S-via <168160925+S-via@users.noreply.github.com>
2024-09-28 14:25:35 -0700

    added into create a neew category category_name and passes in the req.body, same for router.put and router.delete

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res) => {
   // find all categories
   try {
     // be sure to include its associated Products
-    const locationData = await Category.findAll(
+    const categoryData = await Category.findAll(
       {
         include: [{
           model: Product,
@@ -15,7 +15,7 @@ router.get('/', async (req, res) => {
         }]
       }
     );
-    res.status(200).json(locationData);
+    res.status(200).json(categoryData);
   }
   catch (err) {
     res.status(500).json(err);
@@ -26,18 +26,18 @@ router.get('/:id', async (req, res) => {
   try {
     // find one category by its `id` value
     // be sure to include its associated Products
-    const locationData = await Category.findByPk(req.params.id,
+    const categoryData = await Category.findByPk(req.params.id,
       {
         include: [{
           model: Product,
           required: false
         }]
       });
-    if (!locationData) {
+    if (!categoryData) {
       res.status(404).json({ messegae: 'no location with id' });
       return;
     }
-    res.status(200).json(locationData);
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -46,13 +46,15 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new category
   try {
-    const locationData = await Category.create(req.body)
+    const categoryData = await Category.create({
+      category_name:req.body.category_name,
+    });
 
-    if (!locationData) {
+    if (!categoryData) {
       res.status(404).json({ message: 'new category added' });
       return;
     }
-    res.status(200).json(locationData);
+    res.status(200).json(CategoryData);
   }
   catch (err) {
     res.status(500).json(err);
@@ -62,13 +64,19 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try {
-    const locationData = await Category.update(req.params.id)
+    const categoryData = await Category.update({
+      category_name: req.body.category_name},
+      {where:{
+        id: req.params.id,
+      },
+    }
+    );
 
-    if (!locationData) {
-      res.status(404).json({ message: 'category has been updated' });
+    if (!categoryData) {
+      res.status(404).json({ message: 'no category' });
       return;
     }
-    res.status(200).json(locationData);
+    res.status(200).json(categoryData);
   }
   catch (err) {
     res.status(500).json(err);
@@ -78,17 +86,17 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   try {
-    const locationData = await Category.destroy({
+    const categoryData = await Category.destroy({
       where: {
         id: req.params.id
       }
     });
-    if (!locationData) {
+    if (!categoryData) {
       res.status(404).json({ message: 'location removed' });
       return;
 
     }
-    res.status(200).json(locationData);
+    res.status(200).json(categoryData);
   }
   catch (err) {
     res.status(500).json(err);
